f6b241e77bbebd8d8b35371a17b9e5ebadeea791
Rusolea <40546090+Rusolea@users.noreply.github.com>
2023-06-05 16:43:02 -0300

    función más simple con un contador para determinar la posición de inicio y fin de la anteúltima secuencia.

--- src/TP5/Ej17.java
+++ src/TP5/Ej17.java
@@ -1,19 +1,36 @@
 //Hacer un programa que devuelva la posición de inicio y fin de la
 //anteúltima secuencia de números distintos de ceros.
-    package TP5;
 
 import java.util.Random;
 
-public class Ej17 {
-    public static final int MAX = 20, MAXVALOR = 9, MINVALOR = 1;
+public class Ej17_1 {
+    public static final int MAX = 20;
+    public static final int SEPARADOR = 0;
+    public static final int MAXVALOR = 9, MINVALOR = 1;
     public static final double probabilidad_numero = 0.4;
 
     public static void main(String[] args) {
-        int ini=0, fin =0;
-        int arr[] = new int[MAX];
+        int[] arr = new int[MAX];
         cargar_arreglo_aleatorio_secuencias_int(arr);
-        imprimir_arreglo_secuencias_int(arr);
-        obtenerAnteUltimaSecuencia(arr, ini, fin);
+        imprimirSecuencia(arr);
+        posicionSecuencia(arr);
+    }
+    public static void posicionSecuencia(int[] arr) {
+
+        int fin = 0;
+        int ini = MAX;
+        int contador = 0;
+
+        while (ini > 0) {
+            fin = buscarFin(arr, ini - 1);
+            if (fin > 0) {
+                ini = buscarIni(arr, fin);
+                contador++;
+                if (contador == 2) {
+                    imprimirPosiciones(arr, ini, fin);
+                }
+            }
+        }
     }
 
     public static void cargar_arreglo_aleatorio_secuencias_int(int[] arr) {
@@ -29,49 +46,30 @@ public class Ej17 {
         }
     }
 
-    public static void imprimir_arreglo_secuencias_int(int[] arr) {
-        System.out.print("Arreglo de secuencias int\n|");
-        for (int pos = 0; pos < MAX; pos++) {
-            System.out.print(arr[pos] + "|");
-        }
-        System.out.print("\n");
-       
-    }
-
-    public static int obtener_pos_ini(int arr[], int pos) {
-        while (pos > 0 && arr[pos] != 0) {
+    public static int buscarIni(int[] arr, int pos) {
+        while (pos > 0 && arr[pos] != SEPARADOR) {
             pos--;
         }
-        return pos+1;
+        return pos + 1;
     }
 
-    public static int obtener_pos_fin(int[] arr, int pos) {
-        pos=MAX-1;
-        while (pos > 0 && arr[pos] == 0) {
+    public static int buscarFin(int[] arr, int pos) {
+        while (pos > 0 && arr[pos] == SEPARADOR) {
             pos--;
         }
         return pos;
     }
 
-    public static void obtenerAnteUltimaSecuencia(int arr[], int ini, int fin){
-        int anteultimoini=0;
-        int anteultimofin=0;
-        fin=obtener_pos_fin(arr, MAX-1);
-        ini= obtener_pos_ini(arr, fin);
-        int aux= ini-1;
-
-        while(fin>0&&arr[aux]==0){
-            aux--;                      //Calculo anteultimo FIN
+    public static void imprimirSecuencia(int[] arr) {
+        System.out.println("La secuancia es: ");
+        for (int i = 0; i < MAX; i++) {
+            System.out.print(arr[i] + ",");
         }
-        anteultimofin=aux;
-
-        while(ini>0&&arr[aux]!=0){
-            aux--;                      //Calculo anteultimo INICIO
-        }
-
-        anteultimoini=aux+1;
-
-        System.out.println("La anteultima secuencia va de: "+ anteultimoini+ " a: " +anteultimofin);
+        System.out.println();
     }
 
+    public static void imprimirPosiciones(int[] arr, int ini, int fin) {
+        System.out.println("La posicion de inicio  es: " + ini + " y la posicion de fin es: " + fin);
+
+    }
 }
