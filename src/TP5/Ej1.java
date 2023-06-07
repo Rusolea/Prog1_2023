6484d6c99d604ae02745e25fa994057a53edcdbc
Rusolea <40546090+Rusolea@users.noreply.github.com>
2023-06-07 17:43:51 -0300

    Verificar que el valor ingresado
    "Modificado código para verificar que el valor ingresado esté dentro del rango de 1 a 15. Se han agregado constantes VALORMIN y VALORMAX para representar el valor mínimo y máximo permitido en el arreglo."

--- src/TP5/Ej1.java
+++ src/TP5/Ej1.java
@@ -1,37 +1,54 @@
 //Cargar un arreglo de tamaño 15, pidiendo el ingreso por teclado
 //de valores entre 1 y 12. Luego mostrar cómo quedó cargado.
 
-package TP5;
+
+//Cargar un arreglo de tamaño 15, pidiendo el ingreso por teclado
+//de valores entre 1 y 12. Luego mostrar cómo quedó cargado.
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Ej1{
-    public static int MAX = 15;
+public class ej1_1 {
+
+    public static final int MAX = 15;
+    public static final int VALORMIN = 0;
+    public static final int VALORMAX = 15;
+
+
     public static void main(String[] args) {
-       int arr[] = new int [MAX];
-        cargarNumerosArreglo(arr);
+
+        int[] arr = new int[MAX];
+        ingresarValores(arr);
         imprimirArreglo(arr);
     }
 
-
-    public static void cargarNumerosArreglo(int arr[]) { 
+    public static void ingresarValores(int[] arr) {
         BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+
         try {
-            for(int pos=0;pos<MAX;pos++){
-                System.out.println("Ingrese 15 valores y los insertaremos en un arreglo.");
-                arr[pos] = Integer.valueOf(entrada.readLine());
+            for (int i = 0; i < MAX; i++) {
+                System.out.println("Ingrese un valor del 1 al 12 para la posicion " + (i) + ": ");
+                int numero = Integer.parseInt(entrada.readLine());
+
+                while (numero < VALORMIN || numero > VALORMAX) {
+                    System.out.println("Valor incorrecto. Ingrese un valor del 1 al 12 para la posicion " + (i + 1) + ": ");
+                    numero = Integer.parseInt(entrada.readLine());
+
+                }
+                arr[i] = numero;
+
             }
-            
+
+        } catch (Exception e) {
+            System.out.print(e);
         }
 
-        catch (Exception e) {
-            System.out.println(e);
-        }
     }
 
-    public static void imprimirArreglo(int arr[]){
-        for (int pos=0;pos<MAX;pos++){
-            System.out.println("arr[" + pos + "]=>: " + arr[pos]);
+    public static void imprimirArreglo(int[] arr) {
+        for (int i = 0; i < MAX; i++) {
+            System.out.print(arr[i] + "|");
         }
+        System.out.println();
     }
 }
