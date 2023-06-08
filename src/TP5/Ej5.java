fea86b21d7a3e6bc196616e49fd2aa5065f28d2f
Rusolea <40546090+Rusolea@users.noreply.github.com>
2023-06-08 10:34:05 -0300

    Update Ej5.java

--- src/TP5/Ej5.java
+++ src/TP5/Ej5.java
@@ -1,41 +1,42 @@
-package TP5;
-
+//Hacer un programa que dado un arreglo de caracteres de tamaño
+//10 que se encuentra cargado, invierta el orden del contenido. Este
+//intercambio no se debe realizar de manera explícita, hay que
+//hacer un método que incluya una iteración de intercambio.
 import java.util.Random;
-
-public class Ej5 {
-    public static int MAX = 10;
-    public static final int MAXVALOR = 10;
-    public static final int MINVALOR = 1;
-
-    public static void main(String[] args) {
-        char arr[] = new char[MAX];
-        cargar_arreglo_aleatorio_char(arr);
-        imprimir_arreglo_char(arr);
-        intercambioArreglo(arr);
-        System.out.println("\n");
-        imprimir_arreglo_char(arr);
-    }
-
-    public static void cargar_arreglo_aleatorio_char(char[] arr) {
+public class Main{
+    public static final int MAX = 10, MAXVALOR = 10, MINVALOR = 1;
+	public static void main(String[] args) {
+	    int[] arr = new int[MAX];
+	    cargar_arreglo_aleatorio_int(arr);
+	    invertirContenido(arr);
+		
+	}
+	public static void invertirContenido(int[] arr){
+	    int j = MAX-1;
+	    for(int i = 0; i < MAX/2; i++){
+	        int aux = arr[i];
+	        arr[i] = arr[j];
+	        arr[j] = aux;
+	 
+	        j--;
+	    }
+	    imprimirArreglo(arr);
+	    
+	}
+	
+	 public static void cargar_arreglo_aleatorio_int(int[] arr) {
         Random r = new Random();
         for (int pos = 0; pos < MAX; pos++) {
-            arr[pos] = (char) (r.nextInt(26) + 'a');
+            arr[pos] = (r.nextInt(MAXVALOR - MINVALOR + 1) + MINVALOR);
         }
+        imprimirArreglo(arr);
     }
 
-    public static void imprimir_arreglo_char(char[] arr) {
-        for (int pos = 0; pos < MAX; pos++) {
-            System.out.println("nombre_arreglo[" + pos + "]=>: " + arr[pos]);
-        }
-    }
 
-    public static void intercambioArreglo(char[] arr) {
-        char retener;
-        for (int i = 0; i < MAX / 2; i++) {
-            retener = arr[i];
-            arr[i] = arr[(MAX - 1) - i];
-            arr[(MAX - 1) - i] = retener;
+    public static void imprimirArreglo(int[] arr) {
+        for (int i = 0; i < MAX; i++) {
+            System.out.print(arr[i] + "|");
         }
-
+        System.out.println();
     }
 }
