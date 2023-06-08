4ae17e9ce1a5f0b8fbc87257b68b002664b69107
Rusolea <40546090+Rusolea@users.noreply.github.com>
2023-06-08 14:12:23 -0300

    Update Ej9.java

--- src/TP5/Ej9.java
+++ src/TP5/Ej9.java
@@ -1,20 +1,50 @@
-package TP5;
-import java.util.Random;
+//9. Hacer un programa que dado un arreglo de enteros de tamaño 10
+//que se encuentra cargado, solicite al usuario un número entero y
+//lo agregue al principio del arreglo (posición 0). Para ello tendrá
+//que realizar un corrimiento a derecha (se pierde el último valor del
+//arreglo) y colocar el número en el arreglo en la posición indicada.
 
-import java.io.BufferedReader;
+import java.util.Random;
 import java.io.InputStreamReader;
-public class Ej9 {
-    public static int MAX = 10;
-    public static final int MAXVALOR = 10;
-    public static final int MINVALOR = 1;
-    public static final int MIN = 0;
+import java.io.BufferedReader;
+
+public class Ej9_v1 {
+    public static final int MAX = 10, MAXVALOR = 10, MINVALOR = 1, POSICION = 0;
+
     public static void main(String[] args) {
-        int arr[] = new int[MAX];
+        int[] arr = new int[MAX];
+
         cargar_arreglo_aleatorio_int(arr);
+        corrimientoColocarNumero(arr);
+
+    }
+
+    public static void corrimientoColocarNumero(int[] arr) {
+        int numero = solicitarNumero();
+        corrimientoDerecha(arr, numero);
+
+    }
+
+    public static void corrimientoDerecha(int[] arr, int pos) {
+        int j = MAX - 1;
+        for (int i = j; i > POSICION; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[POSICION] = pos;
         imprimirArreglo(arr);
-        int numero= obtenerNuevoNumero();
-        corrimiento_derecha(arr, numero);
-        imprimirArreglo(arr);
+    }
+
+    public static int solicitarNumero() {
+        int numero = -1;
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            System.out.println("Ingrese un numero: ");
+            numero = Integer.parseInt(entrada.readLine());
+        } catch (Exception exc) {
+            System.out.println(exc);
+        }
+        return numero;
     }
 
     public static void cargar_arreglo_aleatorio_int(int[] arr) {
@@ -22,35 +52,14 @@ public class Ej9 {
         for (int pos = 0; pos < MAX; pos++) {
             arr[pos] = (r.nextInt(MAXVALOR - MINVALOR + 1) + MINVALOR);
         }
+        imprimirArreglo(arr);
     }
 
-    public static void imprimirArreglo(int arr[]) {
-        for (int pos = 0; pos < MAX; pos++) {
-            System.out.println("arr[" + pos + "]=>: " + arr[pos]);
+
+    public static void imprimirArreglo(int[] arr) {
+        for (int i = 0; i < MAX; i++) {
+            System.out.print(arr[i] + "|");
         }
+        System.out.println();
     }
-
-    public static int obtenerNuevoNumero() {
-        int numero = 0;
-        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
-        try {
-            System.out.println("Ingrese el numero que quiere ingresar en el arreglo. Se ingresara en la posicion 0.");
-            numero= Integer.valueOf(entrada.readLine());
-        }
-
-        catch (Exception e) {
-            System.out.println(e);
-        }
-        return numero;
-    }
-
-    public static void corrimiento_derecha(int[]arr, int numero){
-        int indice=MAX-1;
-        while(indice>MIN){
-            arr[indice]=arr[indice-1];
-            indice--;
-        }
-        arr[0]=numero;
-    }
-
 }
