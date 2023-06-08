643630a45f92bb0799b1f25a3199cea413484842
Rusolea <40546090+Rusolea@users.noreply.github.com>
2023-06-08 13:44:11 -0300

    Update Ej8.java

--- src/TP5/Ej8.java
+++ src/TP5/Ej8.java
@@ -1,60 +1,36 @@
-package TP5;
-
+//Implementar un método que realice un corrimiento a izquierda en
+//un arreglo ordenado de tamaño MAX=10 a partir de una posición.
 import java.util.Random;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-public class Ej8 {
-    public static int MAX = 10;
-    public static final int MAXVALOR = 10;
-    public static final int MINVALOR = 1;
-    public static final int MIN = 0;
-
+public class Ej8_v1 {
+    public static final int MAX = 10, MAXVALOR = 10, MINVALOR = 1, POSICION = 5;
     public static void main(String[] args) {
-        int arr[] = new int[MAX];
+        int[] arr = new int[MAX];
         cargar_arreglo_aleatorio_int(arr);
-        imprimirArreglo(arr);
-        int pos = obtenerPos();
-        corrimiento_izquierda(arr, pos);
-        System.out.println("Arreglo con corrimiento a izquierda: ");
-        imprimirArreglo(arr);
-
+        corrimientoIzquierda(arr);
     }
+        public static void corrimientoIzquierda(int[] arr){
+            for(int i = POSICION; i < MAX-1; i++){
+                arr[i] = arr[i+1];
 
-    public static void cargar_arreglo_aleatorio_int(int[] arr) {
-        Random r = new Random();
-        for (int pos = 0; pos < MAX; pos++) {
-            arr[pos] = (r.nextInt(MAXVALOR - MINVALOR + 1) + MINVALOR);
+            }
+            imprimirArreglo(arr);
+        }
+
+        public static void cargar_arreglo_aleatorio_int(int[] arr) {
+            Random r = new Random();
+            for (int pos = 0; pos < MAX; pos++) {
+                arr[pos] = (r.nextInt(MAXVALOR - MINVALOR + 1) + MINVALOR);
+            }
+            imprimirArreglo(arr);
+        }
+
+
+        public static void imprimirArreglo(int[] arr) {
+            for (int i = 0; i < MAX; i++) {
+                System.out.print(arr[i] + "|");
+            }
+            System.out.println();
         }
     }
 
-    public static void imprimirArreglo(int arr[]) {
-        for (int pos = 0; pos < MAX; pos++) {
-            System.out.println("arr[" + pos + "]=>: " + arr[pos]);
-        }
-    }
 
-    public static int obtenerPos() {
-        int pos = 0;
-        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
-        try {
-            System.out.println("Ingrese la posicion para hacer el corrimiento a izquierda.");
-            pos= Integer.valueOf(entrada.readLine());
-        }
-
-        catch (Exception e) {
-            System.out.println(e);
-        }
-        return pos;
-    }
-
-    public static void corrimiento_izquierda(int[]arr, int pos){
-        int indice=MIN;
-        while(indice<pos){
-            arr[indice]=arr[indice+1];
-            indice++;
-        }
-
-    }
-}
