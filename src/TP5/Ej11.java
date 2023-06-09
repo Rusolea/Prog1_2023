558f70cc1840218217913bd24edc82e0a7574933
Rusolea <40546090+Rusolea@users.noreply.github.com>
2023-06-09 09:15:27 -0300

    Agrego Consigna Ej11.java

--- src/TP5/Ej11.java
+++ src/TP5/Ej11.java
@@ -1,3 +1,18 @@
+//11. Hacer un programa que dado un arreglo 
+//de enteros de tamaño 10 que se 
+//encuentra precargado, 
+//solicite al usuario un número entero 
+//y elimine todas las ocurrencia de número 
+//en el arreglo. Mientras exista 
+//(en cada iteración tiene que buscar 
+//la posición dentro del arreglo) 
+//tendrá que usar la posición para 
+//realizar un corrimiento a izquierda (
+//quedarán tantas copias de la última
+ 
+ posición del arreglo como cantidad de ocurrencias del número).
+
+
 package TP5;
 import java.util.Random;
 import java.io.BufferedReader;
